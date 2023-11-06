d166b0eeb23fc7c615e73304fe578b08477a9af3
Alexander <super.sany96@yandex.ru>
2023-11-06 21:22:22 +0300

    dfdf

--- script/script.js
+++ script/script.js
@@ -1,22 +1,22 @@
 // Функция, которая обновляет счетчик каждую секунду
 function updateCountdown() {
-    var currentDate = new Date(); // Получаем текущую дату и время
-    var midnight = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1); // Устанавливаем время на полночь следующего дня
-    var timeLeft = midnight - currentDate; // Вычисляем разницу в миллисекундах между полночью и текущим временем
-    var countdown = document.getElementById('countdown'); // Получаем элемент счетчика
+  var currentDate = new Date(); // Получаем текущую дату и время
+  var midnight = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1); // Устанавливаем время на полночь следующего дня
+  var timeLeft = midnight - currentDate; // Вычисляем разницу в миллисекундах между полночью и текущим временем
+  var countdown = document.getElementById('countdown'); // Получаем элемент счетчика
 
-    // Переводим разницу в миллисекундах в часы, минуты и секунды
-    var hours = Math.floor(timeLeft / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  // Переводим разницу в миллисекундах в часы, минуты и секунды
+  var hours = Math.floor(timeLeft / (1000 * 60 * 60));
+  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-    // Обновляем значения элементов счетчика
-    countdown.getElementsByClassName('days')[0].textContent = hours;
-    countdown.getElementsByClassName('hours')[0].textContent = minutes;
-    countdown.getElementsByClassName('minutes')[0].textContent = seconds;
+  // Обновляем значения элементов счетчика
+  countdown.getElementsByClassName('days')[0].textContent = hours;
+  countdown.getElementsByClassName('hours')[0].textContent = minutes;
+  countdown.getElementsByClassName('minutes')[0].textContent = seconds;
 
-    // Вызываем функцию обновления счетчика каждую секунду
-    setTimeout(updateCountdown, 1000);
+  // Вызываем функцию обновления счетчика каждую секунду
+  setTimeout(updateCountdown, 1000);
 }
 
 // Вызываем функцию обновления счетчика в момент загрузки страницы
@@ -25,41 +25,21 @@ window.onload = updateCountdown;
 // Получаем элемент с классом "popup"
 const popup = document.querySelector('.popup');
 
-// Получаем элемент с классом "popup"
-const popup = document.querySelector('.popup');
-
-// Функция для добавления класса "popup-see" к элементу с классом "popup"
-function addPopupSee() {
-  // Проверяем, если разрешение экрана больше или равно 1170px
-  if (window.innerWidth >= 1170) {
-    // Добавляем класс "popup-see" к элементу с классом "popup"
-    popup.classList.add('popup-see');
-  }
-}
-
-// Функция для удаления класса "popup-see" у элемента с классом "popup"
-function removePopupSee() {
-  // Проверяем, если разрешение экрана больше или равно 1170px
-  if (window.innerWidth >= 1170) {
-    // Удаляем класс "popup-see" у элемента с классом "popup"
-    popup.classList.remove('popup-see');
-  }
-}
-
 // Добавляем обработчик события на увод курсора с окна браузера
 window.addEventListener('mouseout', function(event) {
-  // Проверяем, уводит ли курсор за пределы окна браузера
-  if (event.relatedTarget === null) {
-    // Вызываем функцию для добавления класса "popup-see" к элементу с классом "popup"
-    addPopupSee();
-  }
+// Проверяем, уводит ли курсор за пределы окна браузера
+if (event.relatedTarget === null) {
+  // Добавляем класс "popup-see" к элементу с классом "popup"
+  popup.classList.add('popup-see');
+}
 });
 
 // Добавляем обработчик события на возвращение курсора в окно браузера
 window.addEventListener('mouseenter', function(event) {
-  // Вызываем функцию для удаления класса "popup-see" у элемента с классом "popup"
-  removePopupSee();
+// Удаляем класс "popup-see" у элемента с классом "popup"
+popup.classList.remove('popup-see');
 });
+
 
 
 // Получаем элементы с классами "block-img" и "popup"
@@ -69,8 +49,8 @@ const popupe = document.querySelector('.popup');
 // Добавляем обработчик события на клик по элементу с классом "block-img"
 blockImg.forEach(function(element) {
 element.addEventListener('click', function() {
-    // Удаляем класс "popup-see" у элемента с классом "popup"
-    popup.classList.remove('popup-see');
+  // Удаляем класс "popup-see" у элемента с классом "popup"
+  popup.classList.remove('popup-see');
 });
 });
 
@@ -83,32 +63,32 @@ const notificationClasses = ['notification1', 'notification2', 'notification3'];
 
 // Функция для добавления класса notification
 function addNotificationClass() {
-  // Генерируем случайный номер уведомления
-  const randomIndex = Math.floor(Math.random() * notificationClasses.length);
-  
-  // Получаем элемент с случайным классом уведомления
-  const notification = document.querySelector("." + notificationClasses[randomIndex]);
-  
-  // Добавляем класс notification
-  notification.classList.add('notification');
-  
-  // Устанавливаем таймер на удаление класса notification через 10 секунд
-  setTimeout(() => {
-    // Удаляем класс notification
-    notification.classList.remove('notification');
-  }, 10000);
-  
-  // Увеличиваем текущий номер уведомления
-  currentNotification = currentNotification === notificationClasses.length ? 1 : currentNotification + 1;
+// Генерируем случайный номер уведомления
+const randomIndex = Math.floor(Math.random() * notificationClasses.length);
+
+// Получаем элемент с случайным классом уведомления
+const notification = document.querySelector("." + notificationClasses[randomIndex]);
+
+// Добавляем класс notification
+notification.classList.add('notification');
+
+// Устанавливаем таймер на удаление класса notification через 10 секунд
+setTimeout(() => {
+  // Удаляем класс notification
+  notification.classList.remove('notification');
+}, 10000);
+
+// Увеличиваем текущий номер уведомления
+currentNotification = currentNotification === notificationClasses.length ? 1 : currentNotification + 1;
 }
 
 // Функция для перезапуска цикла
 function restartCycle() {
-  // Останавливаем текущий цикл
-  clearInterval(intervalId);
-  
-  // Переустанавливаем цикл с нуля
-  intervalId = setInterval(addNotificationClass, 10000);
+// Останавливаем текущий цикл
+clearInterval(intervalId);
+
+// Переустанавливаем цикл с нуля
+intervalId = setInterval(addNotificationClass, 10000);
 }
 
 // Устанавливаем интервал для добавления класса notification каждые 10 секунд
@@ -116,7 +96,7 @@ let intervalId = setInterval(addNotificationClass, 10000);
 
 // Добавляем обработчик события для перезапуска цикла
 setTimeout(() => {
-  restartCycle();
+restartCycle();
 }, 20000);
 
 
@@ -129,12 +109,12 @@ const crosses = document.querySelectorAll(".notification-cross");
 
 // Для каждого элемента добавляем обработчик события "click"
 crosses.forEach(cross => {
-  cross.addEventListener("click", () => {
-    const notifications = document.querySelectorAll(".notification");
-    
-    // Удаляем класс "notification" у всех элементов с классом "notification"
-    notifications.forEach(notification => {
-      notification.classList.remove("notification");
-    });
+cross.addEventListener("click", () => {
+  const notifications = document.querySelectorAll(".notification");
+  
+  // Удаляем класс "notification" у всех элементов с классом "notification"
+  notifications.forEach(notification => {
+    notification.classList.remove("notification");
   });
+});
 });
